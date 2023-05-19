bd3e1a22be63d4d6d70d2d13faa4b1a80f6a4791
umesh <umesharma.us11@gmail.com>
2023-05-19 22:45:18 +0530

    Implemented delete, update, and error handling to the app

--- part3/phonebook_backend/index.js
+++ part3/phonebook_backend/index.js
@@ -5,6 +5,15 @@ const morgan = require('morgan');
 const cors = require('cors');
 const Person = require('./models/phonebook');
 
+// Error Handling
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message);
+  if (error.name === 'castError') {
+    return res.status(400).send({ error: 'malformatting id' });
+  }
+  next(error);
+};
+
 app.use(express.json());
 app.use(
   morgan(':method :url :status :res[content-length] - :response-time ms :body')
@@ -15,52 +24,42 @@ app.use(express.static('build'));
 morgan.token('body', (req, res) => {
   return JSON.stringify(req.body);
 });
-let phonebook = [
-  {
-    id: 1,
-    name: 'Arto Hellas',
-    number: '040-123456',
-  },
-  {
-    id: 2,
-    name: 'Ada Lovelace',
-    number: '39-44-5323523',
-  },
-  {
-    id: 3,
-    name: 'Dan Abramov',
-    number: '12-43-234345',
-  },
-  {
-    id: 4,
-    name: 'Mary Poppendieck',
-    number: '39-23-6423122',
-  },
-];
 
 app.get('/api/persons', (req, res) => {
   Person.find({}).then((person) => {
-    console.log(1, person);
     res.json(person);
   });
 });
-app.get('/info', (req, res) => {
-  const noOfPeople = Math.max(...phonebook.map((p) => p.id));
+app.get('/info', async (req, res) => {
+  const count = await Person.countDocuments({});
   const currentTime = new Date().toString();
-  res.send(`<p>Phonebook has info for ${noOfPeople} people</br> ${currentTime}</p>
- `);
+  if (count) {
+    res.send(
+      `<p>Phonebook has info for ${count} people</br> ${currentTime}</p> `
+    );
+  } else {
+    res.status(404).end();
+  }
 });
 // singlePerson
-app.get('/api/persons/:id', (req, res) => {
-  Person.findById(req.params.id).then((person) => {
-    res.json(person);
-  });
+app.get('/api/persons/:id', (req, res, next) => {
+  Person.findById(req.params.id)
+    .then((person) => {
+      if (person) {
+        res.json(person);
+      } else {
+        res.status(404).end();
+      }
+    })
+    .catch((error) => next(error));
 });
 // deletePerson
 app.delete('/api/persons/:id', (req, res) => {
-  const id = Number(req.params.id);
-  phonebook = phonebook.filter((person) => person.id !== id);
-  res.status(204).end();
+  Person.findByIdAndRemove(req.params.id)
+    .then((result) => {
+      res.status(204).end();
+    })
+    .catch((error) => next(error));
 });
 
 //Post
@@ -82,6 +81,21 @@ app.post('/api/persons', (req, res) => {
     res.json(savedContact);
   });
 });
+// Update
+app.put('/api/persons/:id', (req, res, next) => {
+  const body = req.body;
+  const person = {
+    name: body.name,
+    number: body.number,
+  };
+  Person.findByIdAndUpdate(req.params.id, person, { new: true })
+    .then((updatedPerson) => {
+      res.json(updatedPerson);
+    })
+    .catch((error) => next(error));
+});
+
+app.use(errorHandler);
 const PORT = process.env.PORT;
 app.listen(PORT, () => {
   console.log(`Server running on port 4000`);
