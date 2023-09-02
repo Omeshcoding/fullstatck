4656f20c5ec82fea323bfe4233576502cdec7f67
umesh <umesharma.us11@gmail.com>
2023-09-02 19:41:55 +0530

    Wrote test to check if returned file is JSON and verified if returned identifier is id

--- part4/blog_list/utils/config.js
+++ part4/blog_list/utils/config.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-undef */
 require('dotenv').config();
 
 // eslint-disable-next-line no-undef
@@ -5,7 +6,6 @@ const PORT = process.env.PORT;
 
 // eslint-disable-next-line no-undef
 const MONGODB_BLOG = process.env.MONGODB_BLOG;
-
 module.exports = {
   MONGODB_BLOG,
   PORT,
