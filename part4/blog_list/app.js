c52d565c4e65ac551157c43d35587d096068f338
umesh <umesharma.us11@gmail.com>
2023-09-13 18:51:52 +0530

    Implemented a route to create new User, and added Schema for user

--- part4/blog_list/app.js
+++ part4/blog_list/app.js
@@ -3,6 +3,7 @@ const app = express();
 const mongoose = require('mongoose');
 const blogRouter = require('./controllers/blogs');
 const config = require('./utils/config');
+const usersRouter = require('./controllers/users');
 
 const url = config.MONGODB_BLOG;
 mongoose.connect(url).then(() => {
@@ -11,5 +12,6 @@ mongoose.connect(url).then(() => {
 
 app.use(express.json());
 app.use('/api/blogs', blogRouter);
+app.use('/api/users', usersRouter);
 
 module.exports = app;
