69011e095912fc766bcedce43a048c7df9dbd6b0
umesh <umesharma.us11@gmail.com>
2024-04-09 16:39:44 +0530

    Separated the action-object function to the action-creator function in the reducer file

---
+++ part6/redux-anecdotes/src/App.jsx
@@ -0,0 +1,42 @@
+import { useSelector, useDispatch } from 'react-redux';
+import { createAnecdote, voteUpdate } from './reducers/anecdoteReducer';
+
+const App = () => {
+  const anecdotes = useSelector((state) => state);
+  const dispatch = useDispatch();
+
+  const addAnecdote = (e) => {
+    e.preventDefault();
+    const anecdote = e.target.anecdote.value;
+    e.target.anecdote.value = '';
+    dispatch(createAnecdote(anecdote));
+  };
+
+  const vote = (id) => {
+    dispatch(voteUpdate(id));
+  };
+  const sortedAnecdote = anecdotes.sort((a, b) => b.votes - a.votes);
+  return (
+    <div>
+      <h2>Anecdotes</h2>
+      {sortedAnecdote.map((anecdote) => (
+        <div key={anecdote.id}>
+          <div>{anecdote.content}</div>
+          <div>
+            has {anecdote.votes}
+            <button onClick={() => vote(anecdote.id)}>vote</button>
+          </div>
+        </div>
+      ))}
+      <h2>create new</h2>
+      <form onSubmit={addAnecdote}>
+        <div>
+          <input name="anecdote" />
+        </div>
+        <button type="submit">create</button>
+      </form>
+    </div>
+  );
+};
+
+export default App;
